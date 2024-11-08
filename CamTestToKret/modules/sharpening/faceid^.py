f1d1991c4520e943c37ee16a548dabe3f35adce3
CruciFixion6 <k0lesnikov.a.d@yandex.ru>
2024-11-08 18:52:02 +0300

    видево

--- CamTestToKret/modules/sharpening/faceid^.py
+++ CamTestToKret/modules/sharpening/faceid^.py
@@ -1,6 +1,20 @@
 from tkinter import *
 from tkinter import messagebox
 
+import cv2
+
+
+cap = cv2.VideoCapture(0)
+
+while True:
+   ret, img = cap.read()
+   cv2.imshow("video", img)
+   if cv2.waitKey(10) == 27:
+       break
+
+cap.release()
+cv2.destroyAllWindows()
+
 def clicked():
     messagebox.showinfo('Ошибка', 'Не подключено')
 
@@ -14,18 +28,18 @@ lbl.place(x=750, y=35)
 lbl1 = Label(window, text = "Должно быть видево", font=("Times New Roman", 14))
 lbl1.grid(column=0, row=0)
 lbl1.place(x=250, y=35)
-btn = Button(window, text="НЧФ",font=("Times New Roman", 14), command = clicked)
-btn.grid(column=0, row=0)
-btn.place(x=750, y= 60)
+btn1 = Button(window, text="НЧФ",font=("Times New Roman", 14), command = clicked)
+btn1.grid(column=0, row=0)
+btn1.place(x=750, y= 60)
 #txt = Entry(window,width=10)(непонятно надо оно или не надо)
 #txt.grid(column=2, row=0)
-btn = Button(window, text="Сохранить фото",font=("Times New Roman", 14), command = clicked)
-btn.grid(column=0, row=0)
-btn.place(x=150, y= 550)
-btn = Button(window, text="Сохранить видео",font=("Times New Roman", 14), command = clicked)
-btn.grid(column=0, row=0)
-btn.place(x=350, y= 550)
-btn = Button(window, text="ВЧФ",font=("Times New Roman", 14), command = clicked)
-btn.grid(column=0, row=0)
-btn.place(x=950, y= 60)
+btn2 = Button(window, text="Сохранить фото",font=("Times New Roman", 14), command = clicked)
+btn2.grid(column=0, row=0)
+btn2.place(x=150, y= 550)
+btn3 = Button(window, text="Сохранить видео",font=("Times New Roman", 14), command = clicked)
+btn3.grid(column=0, row=0)
+btn3.place(x=350, y= 550)
+btn4 = Button(window, text="ВЧФ",font=("Times New Roman", 14), command = clicked)
+btn4.grid(column=0, row=0)
+btn4.place(x=950, y= 60)
 window.mainloop()
